9ccdd5e5e80842b12c99de7b7608c419734c4685
Gregor Weber <gregorwbr@gmail.com>
2017-04-20 14:44:08 +0200

    some progress, though still not working

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,58 @@
 import React from 'react';
 import {graphql, QueryRenderer} from 'react-relay';
-import {Environment, Network} from 'relay-runtime';
+import {Environment, Network, RecordSource, Store,} from 'relay-runtime';
+import 'isomorphic-fetch';
 
 import HelloWorld from './HelloWorld';
 
 const network = Network.create((operation, variables) => (
   fetch('https://api.graph.cool/relay/v1/cixzyedcu0oii0144ltsfckbl', {
     method: 'POST',
-    body: JSON.stringify({query: operation.text, variables})
+    body: JSON.stringify({query: operation.text, variables}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }).then(response => response.json())
 ));
-const environment = new Environment({network});
 
-export default () => (
-  <QueryRenderer
-    environment={environment}
-    query={graphql`
-      query indexQuery {
-        viewer {
-          ...HelloWorld_viewer
-        }
-      }
-    `}
-    render={({error, props}) => error
-      ? <div>{JSON.stringify(error)}</div>
-      : <HelloWorld viewer={props.viewer}/>
+const source = new RecordSource();
+const store = new Store(source);
+const environment = new Environment({network, store});
+
+const query = graphql`
+  query indexQuery {
+    viewer {
+      ...HelloWorld_viewer
     }
-  />
-);
+  }
+`;
+
+export default class extends React.Component {
+
+  static async getInitialProps() {
+    const {
+      createOperationSelector,
+      getOperation,
+    } = environment.unstable_internal;
+    const operation = createOperationSelector(getOperation(query));
+    environment.retain(operation.root);
+    return new Promise((resolve) => environment.streamQuery({
+      operation,
+      onNext: () => resolve(environment.lookup(operation.fragment).data)
+    }));
+  }
+
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={query}
+        render={({error, props}) => error || !props
+          ? <div>{JSON.stringify(error)}</div>
+          : <HelloWorld viewer={props.viewer}/>
+        }
+      />
+    )
+  }
+
+}
